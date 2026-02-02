e667bee156a436791cc29d111513fdc9212ef3db
Ganesh02009 <gkalaskar2009@gmail.com>
2026-02-02 12:15:53 +0530

    Initial commit

---
+++ src/main/java/mypackage/Program.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class Program {
+
+	public void Add() {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Add="+c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Program p =new Program();
+		p.Add();
+	}
+
+}
